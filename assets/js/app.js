0d594f87cfdc95ee3ab7df99989b4e0c245e3e5f
Ana Durand <ana.dc1004@gmail.com>
2017-04-11 11:18:32 -0500

    casi listo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,13 @@ window.addEventListener("load", function(){
   var imagenes = document.getElementsByClassName("modal-img");
   var modalBox = document.getElementById("modal01");
   var img = document.getElementById("img01");
+  var body = document.getElementById("body");
 
   for(i=0; i<imagenes.length; i++){
     imagenes[i].addEventListener("click", function(event){
       event.preventDefault();
+      console.log(event);
+        body.classList.add("hidden");
         img.src = event.target.src;
         modalBox.style.display = "block";
 
@@ -16,6 +19,7 @@ window.addEventListener("load", function(){
 
   closed.addEventListener("click", function(){
     modalBox.style.display = "none";
+    body.classList.remove("hidden");
   });
 
 });
