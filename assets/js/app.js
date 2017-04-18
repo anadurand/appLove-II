bf8040b75dc866fa6bd2e4e8203eaf04d1cf6139
anadurand <ana.dc1004@gmail.com>
2017-04-17 22:22:25 -0500

    refactorizando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+var products = ["Tayman", "Kerala", "The Brass COQ", "Sungenre", "Mistragyna Speciosa", "Foam King", "Volition", "INK","Gilkon", "Owls & Appels", "Sherwood", "Platinum"]
+
+
 window.addEventListener("load", function(){
   var imagenes = document.getElementsByClassName("modal-img");
   var modalBox = document.getElementById("modal01");
@@ -6,6 +9,9 @@ window.addEventListener("load", function(){
   var closed = document.getElementById("close");
   var leftButton = document.getElementById("buttonLeft");
   var rightButton = document.getElementById("buttonRight");
+  var productsBox = document.getElementById("boxProducts");
+  productsBox.innerHTML = "";
+  productsBox.appendChild(mostrar());
 
   for(i=0; i<imagenes.length; i++){
     imagenes[i].addEventListener("click", function(event){
@@ -54,4 +60,38 @@ window.addEventListener("load", function(){
     }
     img.src = ImgUrl[actualImg-1];
   }
+
+
 });
+
+function crearHtml(ruta, nombre){
+
+    var div = document.createElement("div");
+    div.classList.add("products");
+    var img = new Image();
+    img.src = ruta;
+    img.className = "modal-img";
+    var h = document.createElement("h5");
+    h.innerHTML = nombre;
+    h.className = "mt-0";
+
+    div.appendChild(img);
+    div.appendChild(h);
+
+return div;
+
+}
+
+
+function mostrar(){
+  var docF = document.createDocumentFragment();
+
+  products.forEach(function(item,index){
+    var ruta = "assets/img/img-"+ (index + 1) + ".jpg";
+
+    docF.appendChild(crearHtml(ruta, item));
+
+  });
+
+  return docF;
+}
